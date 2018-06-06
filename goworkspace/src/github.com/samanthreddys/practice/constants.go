e7ef5c394d10314e3e42a9b27280cd98e78ce872
Samanth reddy Chinthakuntla <samanthreddys@gmail.com>
2018-06-05 19:06:37 -0500

    iota

---
+++ goworkspace/src/github.com/samanthreddys/practice/constants.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+const p int = 100
+
+const (
+	a  = iota
+	KB = 1 << (iota * 10)
+	MB = 1 << (iota * 10)
+	GB
+	TB
+)
+const (
+	myName = "Reddy"
+	MyID   = "12345"
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(KB)
+	fmt.Println(MB)
+	fmt.Println(GB)
+	fmt.Println(TB)
+}
