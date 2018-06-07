e3657eddd62e70e2d34084ec464318ea360f4f45
Samanth reddy Chinthakuntla <samanthreddys@gmail.com>
2018-06-06 19:59:57 -0500

    loops

---
+++ goworkspace/src/github.com/samanthreddys/practice/remainder.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 51 % 10
+	if x == 1 {
+		fmt.Println("Odd")
+	} else {
+		fmt.Println("Even")
+	}
+	var m int
+	fmt.Println("Enter a number")
+
+	fmt.Scan(&m)
+	for i := 0; i <= m; i++ {
+		if i%2 == 0 {
+			fmt.Println(i, " is Even")
+		} else {
+			fmt.Println(i, " is Odd")
+		}
+	}
+
+}
