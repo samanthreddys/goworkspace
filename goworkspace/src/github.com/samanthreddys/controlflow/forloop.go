c5f3f371a1b2319e8062b4bc5fba2af4fc699e5a
Samanth reddy Chinthakuntla <samanthreddys@gmail.com>
2018-06-10 16:01:30 -0500

    control flow

---
+++ goworkspace/src/github.com/samanthreddys/controlflow/forloop.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 0
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+	j := 0
+	for {
+		j++
+		if j%2 == 0 {
+
+			continue
+
+		}
+		fmt.Println(j)
+		if j >= 50 {
+
+			break
+		}
+
+	}
+
+	fmt.Println("sam")
+}
