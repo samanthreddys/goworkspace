33e3df171a983cff0e23f3236d4fabc9135a4d4b
Samanth reddy Chinthakuntla <samanthreddys@gmail.com>
2018-06-19 20:54:06 -0500

    35multiples

--- goworkspace/src/github.com/samanthreddys/functions/3and5multiples.go
+++ goworkspace/src/github.com/samanthreddys/functions/3and5multiples.go
@@ -2,18 +2,17 @@ package main
 
 import "fmt"
 
-func multiples(x int) int {
-	var sum int
-	for i := 0; i < x; i++ {
-		if (i%5 == 0) || (i%3 == 0) {
-			//fmt.Println(i)
-			sum += i
-
-		}
-	}
-	return sum
-}
-
 func main() {
+	multiples := func(x int) int {
+		var sum int
+		for i := 0; i < x; i++ {
+			if (i%5 == 0) || (i%3 == 0) {
+				//fmt.Println(i)
+				sum += i
+
+			}
+		}
+		return sum
+	}
 	fmt.Println(multiples(1000))
 }
