a63182e2a8de6bfe8d105dab5e070d4519da317a
Samanth reddy Chinthakuntla <samanthreddys@gmail.com>
2018-06-23 23:31:35 -0500

    slices examples

--- goworkspace/src/github.com/samanthreddys/functions/slices1.go
+++ goworkspace/src/github.com/samanthreddys/functions/slices1.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	mySlice := make([]int, 0, 10)
+	mySlice := make([]string, 0, 10)
 	fmt.Println(mySlice)
 	fmt.Println(len(mySlice))
 	fmt.Println(cap(mySlice))
-	for i := 0; i < 180; i++ {
-		mySlice = append(mySlice, i)
-		fmt.Println("Lenght:", len(mySlice), "Capacity:", cap(mySlice), "mySlic:", mySlice[i])
+	for i := 0; i < 1000; i++ {
+		mySlice = append(mySlice, string(i))
+		//fmt.Println("Lenght:", len(mySlice), "Capacity:", cap(mySlice), "mySlic:", mySlice[i])
 	}
+	fmt.Println(mySlice)
 }
