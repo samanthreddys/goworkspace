2df5167460c38c4a523482a146d8999b5b662922
Samanth reddy Chinthakuntla <samanthreddys@gmail.com>
2018-06-23 21:55:56 -0500

    arrays

---
+++ goworkspace/src/github.com/samanthreddys/functions/arrays.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x [58]string
+
+	fmt.Println("array length:", len(x))
+	fmt.Println(x)
+	for i := 65; i <= 122; i++ {
+		x[i-65] = string(i)
+	}
+	fmt.Println(x[42])
+	fmt.Println("array length:", len(x))
+	fmt.Println(x)
+
+}
