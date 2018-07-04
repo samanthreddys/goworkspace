86ed79fbc31bc6f3cc3df81b4ac39e78c4956bf2
Samanth reddy Chinthakuntla <samanthreddys@gmail.com>
2018-07-04 15:11:17 -0500

    structs

--- goworkspace/src/github.com/samanthreddys/playground/structs/structs3.go
+++ goworkspace/src/github.com/samanthreddys/playground/structs/structs3.go
@@ -9,14 +9,40 @@ type Person struct {
 	Age   int
 }
 
+// Company struct
+type Company struct {
+	Person
+	FName string
+	LName string
+	State string
+}
+
 // In below line (p Person) is a reciever for the func
 func (p Person) fullName() string {
 	return p.FName + " " + p.LName
 }
 
+func (c Company) fullName() string {
+	return c.FName + " " + c.LName
+}
+
 func main() {
 	p1 := Person{"Tom", "Doe", 26}
 	p2 := Person{"Jane", "Doe", 25}
+
+	myDetails := Company{
+		Person: Person{
+			FName: "Tim",
+			LName: "Cook",
+			Age:   32,
+		},
+		FName: "John",
+		LName: "Doe",
+		State: "TX",
+	}
+
 	fmt.Println(p1.fullName())
 	fmt.Println(p2.fullName())
+	fmt.Println(myDetails.fullName())
+	fmt.Println(myDetails.Person.fullName())
 }
