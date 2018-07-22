31f91ba6bb38d5c7fdbbe317313054a73b71cc90
Samanth reddy Chinthakuntla <samanthreddys@gmail.com>
2018-07-21 22:32:43 -0500

    g

--- goworkspace/src/github.com/samanthreddys/playground/concurrency/parallelFactorial.go
+++ goworkspace/src/github.com/samanthreddys/playground/concurrency/parallelFactorial.go
@@ -13,8 +13,8 @@ func main() {
 func gen() <-chan int {
 	out := make(chan int)
 	go func() {
-		for i := 0; i < 10; i++ {
-			for j := 3; j < 15; j++ {
+		for i := 0; i < 100; i++ {
+			for j := 3; j < 13; j++ {
 				out <- j
 			}
 		}
